1e8c631c670fce80828bf38a71b60f706d797f7e
ransom <auvergnerw@gmail.com>
2016-01-15 13:53:01 -0800

    sample data to json

--- core/server/models/index.js
+++ core/server/models/index.js
@@ -1,3 +1,4 @@
+var Q = require('q');
 var _ = require('lodash');
 
 var db = require('./db');
@@ -5,6 +6,8 @@ var db = require('./db');
 var _User = require('./user');
 var _Category = require('./category');
 var _Item = require('./item');
+
+var items_data = require('./lots_of_items.json');
 
 // not certain what the "right" level of abstraction is:
 // can database connections live outside an app?
@@ -30,96 +33,30 @@ module.exports.lots_of_items = function () {
   var User = get_model(sqlize, _User);
   var Category = get_model(sqlize, _Category);
   var Item = get_model(sqlize, _Item);
+
+  var make_make_p_item = function (cat_id) {
+    return function (item_data) {
+      return Q().then(function () {
+        return Item.create({
+          title: item_data.title,
+          description: item_data.description,
+          category_id: cat_id
+        });
+      });
+    };
+  };
+
+  var make_p_cat = function (cat_data) {
+    return Q().then(function () {
+      return Category.create({
+        name: cat_data.name
+      });
+    }).then(function (new_cat) {
+      return Q.all(cat_data.items.map(make_make_p_item(new_cat.null)));
+    });
+  };
+
   return sqlize.sync({force: true}).then(function () {
-    return Category.create({
-      name: "Soccer"
-    });
-  }).then(function (catSoccer) {
-    return Item.create({
-      title: "Two shinguards",
-      description: "Prevent injuries resulting from kicks to the shin",
-      category_id: catSoccer.null
-    }).then(function () {
-      return Item.create({
-        title: "Shinguards",
-        description: "Prevent injuries resulting from kicks to the shin",
-        category_id: catSoccer.null
-      });
-    }).then(function () {
-      return Item.create({
-        title: "Jersey",
-        description: "World Cup 2014 commemorative jersey",
-        category_id: catSoccer.null
-      });
-    }).then(function () {
-      return Item.create({
-        title: "Soccer Cleats",
-        description: "Nike cleats",
-        category_id: catSoccer.null
-      });
-    }).done();
-  }).then(function () {
-    return Category.create({
-      name: "Basketball"
-    });
-  }).then(function () {
-    return Category.create({
-      name: "Baseball"
-    });
-  }).then(function (catBaseball) {
-    return Item.create({
-      title: "Bat",
-      description: "Louisville slugger",
-      category_id: catBaseball.null
-    }).done();
-  }).then(function () {
-    return Category.create({
-      name: "Frisbee"
-    });
-  }).then(function (catFrisbee) {
-    return Item.create({
-      title: "Frisbee",
-      description: "A flying disc",
-      category_id: catFrisbee.null
-    }).done();
-  }).then(function () {
-    return Category.create({
-      name: "Snowboarding"
-    });
-  }).then(function (catSnowboarding) {
-    return Item.create({
-      title: "Goggles",
-      description: "Keep the snow out of your eyes",
-      category_id: catSnowboarding.null
-    }).then(function () {
-      return Item.create({
-        title: "Snowboard",
-        description: "Type-A vintage",
-        category_id: catSnowboarding.null
-      });
-    }).done();
-  }).then(function () {
-    return Category.create({
-      name: "Rock Climbing"
-    });
-  }).then(function () {
-    return Category.create({
-      name: "Foosball"
-    });
-  }).then(function () {
-    return Category.create({
-      name: "Skating"
-    });
-  }).then(function () {
-    return Category.create({
-      name: "Hockey"
-    });
-  }).then(function (catHockey) {
-    return Item.create({
-      title: "Stick",
-      description: "A hockey stick",
-      category_id: catHockey.null
-    });
+    return Q.all(items_data.map(make_p_cat));
   });
 };
-
