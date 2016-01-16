538ba53f9b0b537be60a54f68e566c9d66940bba
ransom <auvergnerw@gmail.com>
2016-01-16 00:37:55 -0800

    bug fix

--- core/server/const.js
+++ core/server/const.js
@@ -12,11 +12,7 @@ util.freeze(HTTP_RES_CODE);
 
 var CONST = {};
 
-CONST.HTTP_RES_CODE = {
-  client_err: 400,
-  auth_err: 401,
-  server_err: 500
-};
+CONST.HTTP_RES_CODE = HTTP_RES_CODE;
 
 // CONST.CLIENT_STATIC_URL = CCONST.CLIENT_STATIC_URL;
 // CONST.CLIENT_STATIC_DIR = CCONST.CLIENT_STATIC_DIR;
@@ -25,4 +21,5 @@ CONST.HTTP_RES_CODE = {
 // CONST.AUTH_ENDPOINTS = CCONST.AUTH_ENDPOINTS;
 // CONST.AUTH_BASE = CCONST.AUTH_BASE;
 
-module.exports = util.freeze(_.merge(CCONST, _.cloneDeep(CONST)));
+module.exports = util.freeze(_.merge(_.cloneDeep(CCONST),
+                                     _.cloneDeep(CONST)));
